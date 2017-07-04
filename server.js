5655c969644074bbab62475d858c6544f557f144
Tulio Di Sabatino <tuliosabatino@gmail.com>
2017-07-04 13:26:06 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+  console.log('Node app is running on port ' + port);
+});
